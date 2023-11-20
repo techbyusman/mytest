8cddae157460142db47c832106eed2ca4d11739c
techbyusman <101572200+techbyusman@users.noreply.github.com>
2023-11-20 23:52:55 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 "use strict";
 
 let myButton = document.querySelector('#button1');
-myButton.addeventlistener('click', function(){
-console.log("this is JavaScript");
-)};
+myButton.addeventlistener('click', function({
+  alert("JavaScript is working fine");
+});
